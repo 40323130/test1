157ba2f1c8c04c7e7216d479f66d0a811c8f5ada
40323130 <40323130@gm.nfu.edu.tw>
2016-01-19 23:16:13 +0800

    40323130

--- pelican/publishconf.py
+++ pelican/publishconf.py
@@ -26,7 +26,7 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),
-         ('Python', 'http://python.org/'),(' My Youtube', 'https://www.youtube.com/channel/UCghLzgjr1inu6QYgAYmuGjQ'),(' My Github', 'https://github.com/40323130/test1'),(' My Homework pages', 'http://40323130.github.io/test1/'),
+         ('Python', 'http://python.org/'),('My Youtube', 'https://www.youtube.com/channel/UCghLzgjr1inu6QYgAYmuGjQ'),('My Github', 'https://github.com/40323130/'),('40323130 個人網頁', 'http://40323130.github.io/test1/'),
          ('You can modify those links in your config file', '#'))
 
 # Social widget
